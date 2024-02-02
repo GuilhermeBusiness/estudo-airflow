4b249053a238115b3c658629d2b40772c5608268
Guilherme <guilherme.ribeiro.business@gmail.com>
2024-02-02 07:46:16 -0300

    dummy

---
+++ dags/dummy.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+from airflow.operators.dummy_operator import DummyOperator
+
+dag = DAG('Dummy',
+           description = "Dag run Dag",
+           schedule_interval = None,
+           start_date=datetime(2023,3,5),
+           catchup=False
+           )
+
+task1 = BashOperator(task_id="tsk1", bash_command="sleep 5",dag=dag)
+task2 = BashOperator(task_id="tsk2", bash_command="sleep 5",dag=dag)
+task3 = BashOperator(task_id="tsk3", bash_command="sleep 5",dag=dag)
+task4 = BashOperator(task_id="tsk4", bash_command="sleep 5",dag=dag)
+task5 = BashOperator(task_id="tsk5", bash_command="sleep 5",dag=dag)
+task6 = BashOperator(task_id="tsk6", bash_command="sleep 5",dag=dag)
+
+taskdummy = DummyOperator(task_id="tskdummy",dag=dag)
+
+[task1,task2,task3,task4] >> taskdummy >> [task5,task6]
